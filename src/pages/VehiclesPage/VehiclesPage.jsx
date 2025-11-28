c33cd9c2543581a153418816ce907c2d6bb623b3
Yria Forján Oliveira <yriaforjan.oliveira@gmail.com>
2025-11-28 12:45:41 +0100

    feat(VehiclesPage): fix setVehicles params

--- src/pages/VehiclesPage/VehiclesPage.jsx
+++ src/pages/VehiclesPage/VehiclesPage.jsx
@@ -9,8 +9,8 @@ export default function VehiclesPage() {
   useEffect(() => {
     const load = async () => {
       try {
-        const data = await getVehicles();
-        setVehicles(data?.vehicles || []);//si data es undefined o null, usamos un array vacío
+        const vehicles = await getVehicles();
+        setVehicles(vehicles || []);
       } catch (err) {
         console.error(err);
         setError(err.message || "Error cargando vehículos");
